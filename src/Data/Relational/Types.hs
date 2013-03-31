753c07bdb833c0781a068ed4c8c2c02bbf637f18
John Evans <lgastako@gmail.com>
2013-03-31 05:53:28 -0700

    chkpt

--- src/Data/Relational/Types.hs
+++ src/Data/Relational/Types.hs
@@ -1,31 +1,36 @@
-module Data.Relational.Attribute(AttributeName) where
-
-type TypeName = String
-module Data.Relational.Heading( AttributeName
-                              , TypeName
-                              , Heading
-                              , fromList
-                              , degree
-                              ) where
+module Data.Relational.Types( AttributeName
+                            , TypeName
+                            , Heading
+                            , fromList
+                            , degree
+                            ) where
 
 import qualified Data.Set as Set
 
 -- A heading {H} is a set of ordered pairs or attributes of the form <A,T>, where:
---
+
+data Heading = Heading AttrSet
+    deriving (Show, Eq, Ord)
+
 --     a. A is the name of an attribute of {H}. No two distinct pairs in {H} shall
 --        have the same attribute name.
 --
+
+type AttributeName = String
+
 --     b. T is the name of the declared type of attribute A of {H}.
+
+type TypeName = String
+
 --
 -- The number of pairs in {H}—equivalently, the number of attributes of {H}—is the
 -- degree of {H}.
 
-type TypeName = String
+class Degreed a where
+    degree :: a -> Int
 
-type AttrSet = (Set.Set (AttributeName, TypeName))
 
-data Heading = Heading AttrSet
-    deriving (Show, Eq, Ord)
+
 
 --withSet :: Heading -> (AttrSet -> a) -> a
 --withSet (Heading set) f = f set
@@ -33,8 +38,11 @@ data Heading = Heading AttrSet
 onSet :: (AttrSet -> a) -> Heading -> a
 onSet f (Heading set) = f set
 
-degree :: Heading -> Int
-degree = onSet Set.size
+hdegree :: Heading -> Int
+hdegree = onSet Set.size
+
+instance Degreed Heading where
+    degree = hdegree
 
 --fromList = Heading . Set.fromList
 --it was so pretty!
@@ -96,9 +104,6 @@ type AttributeValue = String -- For now
 
 type AttrSet = (Set.Set (AttributeName, TypeName))
 type AttrValueSet = (Set.Set (AttributeName, TypeName, AttributeValue))
-
-data Heading = Heading AttrSet
-    deriving (Show, Eq, Ord)
 
 --withSet :: Heading -> (AttrSet -> a) -> a
 --withSet (Heading set) f = f set
