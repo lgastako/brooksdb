4ab9827d0d284f8952e6dc9af7cc274d58c2e8ec
John Evans <lgastako@gmail.com>
2013-04-01 22:27:29 -0700

    Moved Temp into main.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,39 @@
 module Main (main) where
 
---import Data.Relation.Types
+import Data.Relation.Types ( Relation
+                           , Tuple
+                           , Heading
+                           , headWith
+                           , withHeading
+                           , fromList
+                           , insertTuple
+                           )
 
-import Temp ( users, otherUsers )
+--db = newDb
+
+heading :: Heading
+heading = fromList [ ("username", "TEXT")
+                   , ("password", "TEXT")
+                   ]
+
+emptyUsers :: Relation
+emptyUsers = withHeading heading
+
+john :: Tuple
+john = headWith heading [ ("username", "john")
+                        , ("password", "letmein")
+                        ]
+
+bob :: Tuple
+bob = headWith heading [ ("username", "bob")
+                       , ("password", "letmein, also")
+                       ]
+
+users :: Relation
+users = insertTuple emptyUsers john
+
+otherUsers :: Relation
+otherUsers = insertTuple users bob
 
 
 main :: IO ()
