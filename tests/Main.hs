b03c9268d5838f7a5bebfd91aea4704cb8e06f6f
John Evans <lgastako@gmail.com>
2013-04-04 02:17:58 -0700

    Checkpoint fix of some problems, upgrade to newer cabal for happy/alex.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,49 +1,17 @@
 module Main where
 
--- import Control.Monad ( liftM )
-
 import Test.Framework ( Test
---                      , TestName
                       , defaultMain )
---import Test.Framework.Providers.QuickCheck2 ( testProperty )
---import Test.QuickCheck ( Testable, Arbitrary, choose )
---import Test.QuickCheck.Arbitrary ( arbitrary )
+import Test.Framework.Providers.HUnit ( testCase )
+import Test.HUnit ( assertEqual )
 
---import Data.Relation.Types
+import Language.Heidi.Lexer ( Var )
 
---import System.Random ( StdGen, mkStdGen )
-
-
--- instance Arbitrary Heading where
---     arbitrary = do len <- choose (1, 512)
---                    -- anames <- take len (repeat arbitrary)
---                    -- tnames <- [arbitrary | _ <- [0..len]]
---                    -- return (fromList $ (zip anames tnames))
---                    xs <- [(arbitrary, arbitrary) | _ <- [0..len]]
---                    liftM fromList xs
-
-
--- prop_tuple_degree :: (Degreed a, Headed a, Arbitrary a, Show a, Testable prop) =>
---                       a -> Testable prop
--- prop_tuple_degree :: Tuple -> Bool
---prop_tuple_degree t = tupleDegree == headingDegree
-    --where
-        --tupleDegree = degree t
-        --headingDegree = degree h
-        --h = heading t
-
------------------------------------------------------------------------------
-
-
---names :: Testable a => [(TestName, a)]
---names = [ ("tmp", prop_tuple_degree) ]
---names = []
+test_lex_var = assertEqual "var > Var" Var (alexScanTokens "var")
 
 tests :: [Test]
---tests = map (uncurry testProperty) names
-
---tests = [((uncurry testProperty) ("tuple degree", prop_tuple_degree))]
-tests = []
+tests = [ test_lex_var
+        ]
 
 main :: IO ()
 main = defaultMain tests
