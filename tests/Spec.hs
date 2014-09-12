89dc867eb780dc9b66c45be0390c48196b879123
John Evans <lgastako@gmail.com>
2014-09-11 23:40:05 -0700

    Less.

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -10,45 +10,32 @@ import Data.Relation.Types ( fromList
 main :: IO ()
 main = hspec $ do
   describe "asKeyDefList" $ do
-
     context "empty set"  $ do
-
       let heading0 = fromList []
-
       it "returns an empty string" $ do
-
         (asKeyDefList heading0) `shouldBe` ""
 
     context "one item" $ do
-
       let heading1 = fromList [ ("foo", "String") ]
-
       it "returns a single keyDef" $ do
-
         (asKeyDefList heading1) `shouldBe` "\n    foo String\n"
 
     context "two items" $ do
-
       let heading2 = fromList [ ("foo", "String")
                               , ("bar", "Int")
                               ]
-
       it "returns a single keyDef" $ do
-
         (asKeyDefList heading2) `shouldBe` (  "\n"
                                            ++ "    bar Int,\n"
                                            ++ "    foo String\n"
                                            )
 
     context "three items" $ do
-
       let heading3 = fromList [ ("foo", "String")
                               , ("bar", "Int")
                               , ("baz", "Bool")
                               ]
-
       it "returns a single keyDef" $ do
-
         (asKeyDefList heading3) `shouldBe` (  "\n"
                                            ++ "    bar Int,\n"
                                            ++ "    baz Bool,\n"
